0174a8afa3c0cfc0b5470353796bb6edb45b2bd1
ajithkumar2498 <ajibhc24@gmail.com>
2026-01-09 03:45:54 +0530

    url updated

--- src/utils/Api.jsx
+++ src/utils/Api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: import.meta.env.VITE_API_URL, withCredentials: true });
+const API = axios.create({ baseURL: import.meta.env.VITE_API_URL });
 
 
 API.interceptors.request.use((req) => {
